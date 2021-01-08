51c906a7c086053237f6a1e51b9baea0d7ad7602
hiimbalii <tcrbt1@gmail.com>
2021-01-08 17:27:24 +0100

    :sapkrles: adds CRD to forms

--- src/forms/forms.module.ts
+++ src/forms/forms.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Form, FormSchema } from './form';
 import { FormsController } from './forms.controller';
 import { FormsService } from './forms.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Form.name, schema: FormSchema }]),
+  ],
   controllers: [FormsController],
-  providers: [FormsService]
+  providers: [FormsService],
 })
 export class FormsModule {}
--- src/forms/forms.service.ts
+++ src/forms/forms.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateFormDTO } from './dto/createFormDto';
+import { SubmitFormDTO } from './dto/submitFormDTO';
+import { Form, FormDocument } from './form';
 
 @Injectable()
-export class FormsService {}
+export class FormsService {
+  constructor(@InjectModel(Form.name) private formModel: Model<FormDocument>) {}
+
+  async getFormForCar(url: string) {
+    return this.formModel.findOne({ autoUrl: url }).exec();
+  }
+
+  async createFrom(createFormDTO: CreateFormDTO) {
+    const newForm = new this.formModel(createFormDTO);
+    return newForm.save();
+  }
+  async deleteForm(id: string) {
+    return await this.formModel.findByIdAndDelete(id).exec();
+  }
+}
