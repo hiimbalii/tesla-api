eb203575c12bc4e7ea4d0516134018ac4c720b9a
hiimbalii <tcrbt1@gmail.com>
2021-01-08 16:25:06 +0100

    :sparkles: Connects to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [CarsModule],
+  imports: [
+    CarsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.tz5ao.mongodb.net/tesla?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
